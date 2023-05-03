7f266e10dc8468e4d887b9ea83b3434fe3893c9f
iuri sajaia <sajaiaiuri@gmail.com>
2023-05-03 21:04:50 +0400

    test

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>copyright 2023</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <ul className="li">Home</ul>
+      <ul className="li">About</ul>
+      <ul className="li">Blog</ul>
+      <ul className="li">Contact</ul>
+      <ul className="li">See More</ul>
+    </header>
+  );
+}
+
+export default Header;
